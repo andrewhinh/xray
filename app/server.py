70cb137d11112b4501517a03f9839c97f06dc645
AndrewJHinh <40700820+AndrewJHinh@users.noreply.github.com>
2020-08-17 14:41:11 -0700

    Update server.py

--- app/server.py
+++ app/server.py
@@ -49,6 +49,8 @@ async def download_file(url, dest):
 
 async def setup_learner():
     await download_file(export_file_url, path / export_file_name)
+    _sys_info = pickle_module.load(f,encoding='latin1')
+    unpickler = pickle_module.Unpickler(f,encoding='latin1')
     try:
         learn = load_learner(path, export_file_name)
         return learn
